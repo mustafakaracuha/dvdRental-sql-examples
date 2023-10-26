5fc2ef54bf4b23233ec3df7421b45a44e792e40e
mustafakaracuha <mustafa.karacuha@detaysoft.com>
2023-10-26 13:57:07 +0300

    table added

---
+++ TABLE/example.sql
@@ -0,0 +1,21 @@
+UPDATE employee
+SET name = 'Mustafa'
+WHERE id > 48
+
+UPDATE employee
+SET name = 'Mustafa'
+WHERE id = 2
+
+UPDATE employee
+SET email = 'karacuhaa58@gmail.com'
+WHERE name = 'Mustafa'
+
+UPDATE employee
+SET name = 'Ahmet'
+WHERE name = 'Mustafa'
+
+DELETE FROM employee
+WHERE name = 'Ahmet'
+
+DELETE FROM employee
+WHERE id > 30 
